529ffc530e488acc433405b07e48c4bb7be500ee
joule.io <162421658+jouleee@users.noreply.github.com>
2025-05-11 20:59:27 +0700

    update database

--- mvp_php/mvp_php.sql
+++ mvp_php/mvp_php.sql
@@ -1,43 +1,79 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2025 at 03:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-Source Server         : local
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : mvp_php
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2025-05-02 11:53:37
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `mahasiswa`
--- ----------------------------
-DROP TABLE IF EXISTS `mahasiswa`;
+--
+-- Database: `mvp_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
 CREATE TABLE `mahasiswa` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nim` varchar(100) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `tempat` varchar(100) NOT NULL,
   `tl` varchar(100) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `telp` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `telp` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- ----------------------------
--- Records of mahasiswa
--- ----------------------------
-INSERT INTO `mahasiswa` VALUES ('1', '2202123', 'Abdullah', 'Garut', '2020-12-11', 'Laki-laki', 'abdul@upi.edu', '088970803025');
-INSERT INTO `mahasiswa` VALUES ('2', '2202124', 'Wahyu', 'Cimahi', '2020-12-14', 'Laki-laki', 'wah@upi.edu', '089678898545');
-INSERT INTO `mahasiswa` VALUES ('4', '2202125', 'Ayang', 'Bandung', '2020-11-29', 'Perempuan', 'ay@gmail.com', '081321778980');
-INSERT INTO `mahasiswa` VALUES ('5', '2202126', 'Rakha', 'Palembang', '2021-01-04', 'Laki-laki', 'palembang@gmail.com', '088970803025');
-INSERT INTO `mahasiswa` VALUES ('6', '2202127', 'Prilla', 'Seoul', '2001-05-05', 'Perempuan', 'prillarosaria@upi.edu', '081234876235');
-INSERT INTO `mahasiswa` VALUES ('7', '2202128', 'Son', 'Canada', '1994-02-21', 'Perempuan', 'chrstjsmn@gmail.com', '081573038425');
-INSERT INTO `mahasiswa` VALUES ('8', '2202129', 'Jeno', 'Incheon', '2000-12-23', 'Laki-laki', 'jeno@gmail.com', '08138746239');
-INSERT INTO `mahasiswa` VALUES ('9', '2202130', 'Mark', 'Canada', '1999-08-20', 'Laki-laki', 'mark@upi.edu', '08237218473');
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `tempat`, `tl`, `gender`, `email`, `telp`) VALUES
+(1, '2202123', 'Abdullah', 'Garut', '2020-12-11', 'Laki-laki', 'abdullah@upi.edu', '088970803025'),
+(2, '2202124', 'Wahyu', 'Cimahi', '2020-12-14', 'Laki-laki', 'wah@upi.edu', '089678898545'),
+(4, '2202125', 'Ayang', 'Bandung', '2020-11-29', 'Perempuan', 'ay@gmail.com', '081321778980'),
+(5, '2202126', 'Rakha', 'Palembang', '2021-01-04', 'Laki-laki', 'palembang@gmail.com', '088970803025'),
+(6, '2202127', 'Prilla', 'Seoul', '2001-05-05', 'Perempuan', 'prillarosaria@upi.edu', '081234876235'),
+(7, '2202128', 'Sonoooo', 'Canada', '1994-02-21', 'Perempuan', 'chrstjsmn@gmail.com', '081573038425'),
+(8, '2202129', 'Jeno', 'Incheon', '2000-12-23', 'Laki-laki', 'jeno@gmail.com', '08138746239'),
+(17, '2300484', 'Julian Dwi Satrio', 'Cilegon', '2005-07-03', 'Laki-laki', 'dwisatrio.julian@gmail.com', '081322503073'),
+(19, '2202123', 'Ujang Keduuuu', 'Garut', '2004-03-20', 'Perempuan', 'dpmbkemakom@gmail.com', '088970803025');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
